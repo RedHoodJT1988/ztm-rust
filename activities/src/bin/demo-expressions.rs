928b166a2f2bd62e7e790621122eee96bc5a78fd
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-12 10:40:02 -0500

    completed expressions demo

--- activities/src/bin/demo-expressions.rs
+++ activities/src/bin/demo-expressions.rs
@@ -1,1 +1,17 @@
-fn main() {}
+enum Access {
+    Admin,
+    Manager,
+    User,
+    Guest
+}
+
+fn main() {
+    // secret file: admins only
+    let access_level = Access::Guest;
+    let can_access_file = match access_level {
+        Access::Admin => true,
+        _ => false,
+    };
+
+    println!("{:?}", can_access_file);
+}
