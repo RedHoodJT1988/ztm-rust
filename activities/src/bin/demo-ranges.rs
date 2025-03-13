602dc0ce706afcf6212fe095f245b3bb088689ec
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-13 12:01:44 -0500

    completed ranges demo

--- activities/src/bin/demo-ranges.rs
+++ activities/src/bin/demo-ranges.rs
@@ -1,1 +1,14 @@
-fn main() {}
+#![allow(unused_variables)]
+
+fn main() {
+    let range = 1..=3;
+    let range = 1..4;
+
+    for num in 1..4 {
+        println!("{num:?}");
+    }
+
+    for ch in 'a'..='f' {
+        println!("{ch:?}")
+    }
+}
