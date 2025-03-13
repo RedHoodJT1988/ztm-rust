d2c5fb36e9591703e75c1bf9bf4e17d0dd01a80f
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-13 12:00:05 -0500

    completed custom error types demo

--- activities/src/bin/demo-custom-error-types.rs
+++ activities/src/bin/demo-custom-error-types.rs
@@ -1,5 +1,17 @@
+#![allow(dead_code)]
+
 use chrono::{DateTime, Duration, Utc};
 use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum PassError {
+    #[error("expired pass")]
+    PassExpired,
+    #[error("insufficient funds: {0}")]
+    InsufficientFunds(isize),
+    #[error("pass read error: {0}")]
+    ReadError(String),
+}
 
 struct SubwayPass {
     id: usize,
@@ -19,14 +31,22 @@ fn swipe_card() -> Result<SubwayPass, PassError> {
 fn use_pass(pass: &mut SubwayPass, cost: isize) -> Result<(), PassError> {
     if Utc::now() > pass.expires {
         Err(PassError::PassExpired)
+    } else if pass.funds - cost < 0 {
+        Err(PassError::InsufficientFunds(pass.funds))
     } else {
-        if pass.funds - cost < 0 {
-            Err(PassError::InsufficientFunds(pass.funds))
-        } else {
-            pass.funds = pass.funds - cost;
-            Ok(())
-        }
+        pass.funds -= -cost;
+        Ok(())
     }
 }
 
-fn main() {}
+fn main() {
+    let can_board = swipe_card().and_then(|mut pass| use_pass(&mut pass, 3));
+    match can_board {
+        Ok(_) => println!("ok to board"),
+        Err(e) => match e {
+            PassError::ReadError(_) => (),
+            PassError::PassExpired => (),
+            PassError::InsufficientFunds(_) => (),
+        },
+    };
+}
