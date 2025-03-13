bf3ea6a9cb8422431bc2761b3356454ba20bcd34
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-13 13:28:36 -0500

    completed vector basics demo

--- activities/src/bin/demo-vector-basics.rs
+++ activities/src/bin/demo-vector-basics.rs
@@ -1,1 +1,16 @@
-fn main() {}
+struct Test {
+    score: i32,
+}
+
+fn main() {
+    let my_scores = vec![
+        Test { score: 90 },
+        Test { score: 88 },
+        Test { score: 77 },
+        Test { score: 93 },
+    ];
+
+    for test in my_scores {
+        println!("score = {:?}", test.score);
+    }
+}
