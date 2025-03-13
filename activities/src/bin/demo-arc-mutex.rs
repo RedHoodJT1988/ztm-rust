e92a073b826c771a64a3148ed916b59aa16ba496
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-13 11:58:51 -0500

    completed arc mutex demo

--- activities/src/bin/demo-arc-mutex.rs
+++ activities/src/bin/demo-arc-mutex.rs
@@ -8,12 +8,32 @@ type SharedSignData = Arc<Mutex<String>>;
 struct DigitalSignBoard {
     display: SharedSignData,
 }
-
-fn spawn_display_thread(display_data: SharedSignData) {
-    thread::spawn(|| {});
+impl DigitalSignBoard {
+    fn update(&self) {
+        let data = self.display.lock();
+        println!("sign data = '{}'", data);
+        /* code to push to sign */
+    }
 }
 
-fn change_data(display_data: SharedSignData, new_data: &str) {}
+fn spawn_display_thread(display_data: SharedSignData) {
+    thread::spawn(|| {
+        let board = DigitalSignBoard {
+            display: display_data,
+        };
+
+        loop {
+            board.update();
+            thread::sleep(Duration::from_millis(200));
+        }
+    });
+}
+
+fn change_data(display_data: SharedSignData, new_data: &str) {
+    let mut data = display_data.lock();
+    *data = new_data.to_owned();
+    println!("--------------updated:{new_data}");
+}
 
 fn main() {
     let display_data = Arc::new(Mutex::new("initial".to_owned()));
