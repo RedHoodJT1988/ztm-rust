d5e6cf552d919f6f35b05af7bee8ec5d00212311
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-13 12:57:44 -0500

    completed activity a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,5 +10,40 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+struct ShippingBox {
+    dimensions: (i32, i32, i32),
+    weight: i32,
+    color: BoxColor,
+}
+
+#[derive(Debug)]
+enum BoxColor {
+    Brown,
+    Black,
+    White,
+}
+
+impl ShippingBox {
+    fn new(dimensions: (i32, i32, i32), weight: i32, color: BoxColor) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color,
+        }
+    }
+
+    fn print_characteristics(&self) {
+        println!("dimensions: {:?}", self.dimensions);
+        println!("weight: {:?}", self.weight);
+        println!("color: {:?}", self.color);
+    }
+}
+fn main() {
+    let box1 = ShippingBox::new((10, 10, 10), 5, BoxColor::Brown);
+    box1.print_characteristics();
+    let box2 = ShippingBox::new((20, 20, 20), 10, BoxColor::Black);
+    box2.print_characteristics();
+    let box3 = ShippingBox::new((30, 30, 30), 15, BoxColor::White);
+    box3.print_characteristics();
+}
 
