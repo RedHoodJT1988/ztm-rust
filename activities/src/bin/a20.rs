43788b2c426bf38e866db16c663727de0c58b3f6
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-24 11:17:28 -0500

    Completed activity a20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -23,5 +23,43 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+// * Use an enum to store the possible power states
+enum PowerState {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+// * Use a function with a match expression to print out the power messages
+fn print_power_message(power_state: PowerState) {
+    match power_state {
+        PowerState::Off => println!("Turning off"),
+        PowerState::Sleep => println!("Going to sleep"),
+        PowerState::Reboot => println!("Rebooting"),
+        PowerState::Shutdown => println!("Shutting down"),
+        PowerState::Hibernate => println!("Hibernating"),
+    }
+}
+fn main() {
+    // * Use a match expression to convert the user input into the power state enum
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let power_state = match input.trim().to_lowercase().as_str() {
+        "off" => PowerState::Off,
+        "sleep" => PowerState::Sleep,
+        "reboot" => PowerState::Reboot,
+        "shutdown" => PowerState::Shutdown,
+        "hibernate" => PowerState::Hibernate,
+        _ => {
+            println!("Invalid power state");
+            return;
+        }
+    };
+
+    print_power_message(power_state);
+}
 
