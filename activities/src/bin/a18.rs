6471592eae5bd14bae69d993bcd1f8101b6ad2c8
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-24 10:48:34 -0500

    Completed activity a18

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -16,5 +16,36 @@
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
 
-fn main() {}
+#[derive(Debug)]
+struct Adult {
+    age: u8,
+    name: String,
+}
+
+impl Adult {
+    fn new(age: u8, name: &str) -> Result<Self, &str> {
+        if age >= 21 {
+            Ok(Self {
+                age, name: name.to_string(),
+            })
+        } else {
+            Err("Age must be at least 21")
+        }
+    }
+}
+
+fn main() {
+    let child = Adult::new(15, "Justin");
+    let adult = Adult::new(21, "Tommy");
+
+    match child {
+        Ok(child) => println!("{} is {} years old", child.name, child.age),
+        Err(e) => println!("{e}"),
+    }
+
+    match adult {
+        Ok(adult) => println!("{} is {} years old", adult.name, adult.age),
+        Err(e) => println!("{e}"),
+    }
+}
 
