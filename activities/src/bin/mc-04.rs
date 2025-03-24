556ae42327a93f0d29061680eeb77c3042923f3f
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-24 16:20:43 -0500

    Completed managed code activity mc-04

--- activities/src/bin/mc-04.rs
+++ activities/src/bin/mc-04.rs
@@ -21,14 +21,114 @@
 // - The `.max()` method on iterators won't work for f64. Consider writing a `for` loop and
 //   manually track the highest temperature, or use `.fold`
 
+#![allow(dead_code)]
+
 fn main() {}
+
+#[derive(Debug, Default)]
+struct TemperatureSensor {
+    readings: Vec<f64>,
+}
+
+impl TemperatureSensor {
+    pub fn is_empty(&self) -> bool {
+        self.readings.is_empty()
+    }
+
+    fn record_temperature(&mut self, temp: f64) {
+        self.readings.push(temp);
+    }
+
+    fn get_average_temperature(&self) -> Option<f64> {
+        (!self.readings.is_empty())
+            .then_some(self.readings.iter().copied().sum::<f64>() / self.readings.len() as f64)
+    }
+
+    fn get_max_temperature(&self) -> Option<f64> {
+        (!self.readings.is_empty()).then_some(
+            self.readings
+                .iter()
+                .copied()
+                .fold(f64::NEG_INFINITY, |a, b| a.max(b)),
+        )
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    trait TemperatureSensorTestExt {
+        fn with_temps(temps: &[f64]) -> TemperatureSensor;
+    }
+
+    impl TemperatureSensorTestExt for TemperatureSensor {
+        fn with_temps(temps: &[f64]) -> TemperatureSensor {
+            let mut sensor = TemperatureSensor::default();
+            for temp in temps {
+                sensor.record_temperature(*temp);
+            }
+            sensor
+        }
+    }
+
     #[test]
-    fn feature() {
-        todo!()
+    fn new_temperature_sensor_is_empty() {
+        let sensor = TemperatureSensor::default();
+        assert!(sensor.is_empty());
+    }
+
+    #[test]
+    fn records_a_temperature() {
+        let mut sensor = TemperatureSensor::default();
+
+        sensor.record_temperature(30.0);
+
+        assert!(!sensor.is_empty());
+    }
+
+    #[test]
+    fn returns_average_temperature_with_no_readings() {
+        let sensor = TemperatureSensor::default();
+
+        let avg = sensor.get_average_temperature();
+
+        assert!(avg.is_none());
+    }
+
+    #[test]
+    fn returns_average_temperature_with_one_reading() {
+        let sensor = TemperatureSensor::with_temps(&[10.0]);
+
+        let avg = sensor.get_average_temperature();
+
+        assert_eq!(avg, Some(10.0));
+    }
+
+    #[test]
+    fn returns_average_temperature_with_two_readings() {
+        let sensor = TemperatureSensor::with_temps(&[10.0, 20.0]);
+
+        let avg = sensor.get_average_temperature();
+
+        assert_eq!(avg, Some(15.0));
+    }
+
+    #[test]
+    fn max_temp_returns_none_when_no_readings_are_present() {
+        let sensor = TemperatureSensor::default();
+
+        let max = sensor.get_max_temperature();
+
+        assert!(max.is_none());
+    }
+
+    #[test]
+    fn returns_max_temperature_with_two_readings() {
+        let sensor = TemperatureSensor::with_temps(&[20.0, 10.0]);
+
+        let max = sensor.get_max_temperature();
+
+        assert_eq!(max, Some(20.0));
     }
 }
