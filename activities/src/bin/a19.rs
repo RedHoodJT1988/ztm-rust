b23b08a30c177cc2972d0267cd1b9dfaeba101ad
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-24 10:59:19 -0500

    Completed activity a19

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,5 +13,24 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chairs", 5);
+    stock.insert("Beds", 3);
+    stock.insert("Tables", 2);
+    stock.insert("Couches", 0);
+
+    for (item, count) in &stock {
+        if *count == 0 {
+            println!("{}: Out of stock", item);
+        } else {
+            println!("{}: {}", item, count);
+        }
+    }
+
+    let total: i32 = stock.values().sum();
+    println!("Total items in stock: {}", total);
+}
 
